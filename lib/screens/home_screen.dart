e7d9ca6f4a4e1e096c024c2c07da0e2ff55008d1
edmond-bobiles <edmondjohn.bobiles@gmail.com>
2025-11-17 16:56:03 +0800

    Added bottom nav to homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  // Placeholder pages
+  final List<Widget> _pages = [
+    Center(child: Text("Home")),
+    Center(child: Text("Transactions")),
+    Center(child: Text("Add")),
+    Center(child: Text("Budgets")),
+    Center(child: Text("Accounts")),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Home Screen (Navigation will be added here)",
-          style: TextStyle(fontSize: 18),
-        ),
+      body: _pages[_selectedIndex],
+
+      // Bottom Navigation
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: "Transactions",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            label: "Add",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart_outline),
+            label: "Budgets",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: "Accounts",
+          ),
+        ],
       ),
     );
   }
